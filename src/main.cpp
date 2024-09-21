2667dfdd60afde93da9cb9716845729f84dc0d7d
Ismael <ijimenezm@hotmail.es>
2024-09-21 16:33:55 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,100 @@
 #include <iostream>
 
-///////
+// ///////
 
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
-enum class ColorId
-{
-    red,
-    green,
-    cyan,
-};
+// enum class ColorId
+// {
+//     red,
+//     green,
+//     cyan,
+// };
 
-enum class ColorFont
-{
-    regular,
-    bold,
-};
+// enum class ColorFont
+// {
+//     regular,
+//     bold,
+// };
 
-std::string color(ColorId color_id, const std::string& line, ColorFont font = ColorFont::regular)
-{
+// std::string color(ColorId color_id, const std::string& line, ColorFont font = ColorFont::regular)
+// {
 
-#ifdef _WIN32
-#include <windows.h>
+// #ifdef _WIN32
+// #include <windows.h>
 
-    HANDLE h_console{GetStdHandle(STD_OUTPUT_HANDLE)};
-    CONSOLE_SCREEN_BUFFER_INFO console_info;
-    WORD saved_attributes;
+//     HANDLE h_console{GetStdHandle(STD_OUTPUT_HANDLE)};
+//     CONSOLE_SCREEN_BUFFER_INFO console_info;
+//     WORD saved_attributes;
     
-    // Save current console attributes
-    GetConsoleScreenBufferInfo(h_console, &console_info);
-    saved_attributes = console_info.wAttributes;
+//     // Save current console attributes
+//     GetConsoleScreenBufferInfo(h_console, &console_info);
+//     saved_attributes = console_info.wAttributes;
 
-    WORD color_code{saved_attributes}
+//     WORD color_code{saved_attributes}
 
-    switch (color_id) {
-        case ColorId::red    color_code = FOREGROUND_RED; break;
-        case ColorId::green: color_code = FOREGROUND_GREEN; break;
-        case ColorId::cyan:  color_code = FOREGROUND_GREEN | FOREGROUND_BLUE; break;
-    }
+//     switch (color_id) {
+//         case ColorId::red    color_code = FOREGROUND_RED; break;
+//         case ColorId::green: color_code = FOREGROUND_GREEN; break;
+//         case ColorId::cyan:  color_code = FOREGROUND_GREEN | FOREGROUND_BLUE; break;
+//     }
 
-    SetConsoleTextAttribute(h_console, color_code);
+//     SetConsoleTextAttribute(h_console, color_code);
 
-    std::cout << line;
+//     std::cout << line;
 
-    SetConsoleTextAttribute(h_console, saved_attributes);
+//     SetConsoleTextAttribute(h_console, saved_attributes);
 
-#else
-    std::string color_code = "\033[";
+// #else
+//     std::string color_code = "\033[";
 
-    std::string color_font = "0";
+//     std::string color_font = "0";
 
-    if (font == ColorFont::bold) {
-        color_font = "1";
-    }
+//     if (font == ColorFont::bold) {
+//         color_font = "1";
+//     }
 
-    color_code += color_font;
-    color_code += ';';
+//     color_code += color_font;
+//     color_code += ';';
 
-    switch (color_id) {
-        case ColorId::red:    color_code += "31"; break;
-        case ColorId::green:  color_code += "32"; break;
-        case ColorId::cyan:   color_code += "36"; break;
-        default:              color_code += "0"; break;
-    }
+//     switch (color_id) {
+//         case ColorId::red:    color_code += "31"; break;
+//         case ColorId::green:  color_code += "32"; break;
+//         case ColorId::cyan:   color_code += "36"; break;
+//         default:              color_code += "0"; break;
+//     }
 
-    color_code += "m";
+//     color_code += "m";
 
-    std::cout << color_code << line << "\033[0m";
+//     std::cout << color_code << line << "\033[0m";
     
-#endif
+// #endif
 
-    return "";
-}
+//     return "";
+// }
 
-//////
+// //////
 
-void show_help()
-{
-    std::cout << "C++'s package manager\n\n";
+// void show_help()
+// {
+//     std::cout << "C++'s package manager\n\n";
 
-    std::cout << color(ColorId::green, "Usage:", ColorFont::bold) << color(ColorId::cyan, " cask [COMMAND]", ColorFont::bold) << "\n\n";
+//     std::cout << color(ColorId::green, "Usage:", ColorFont::bold) << color(ColorId::cyan, " cask [COMMAND]", ColorFont::bold) << "\n\n";
 
-    std::cout << color(ColorId::green, "Commands:", ColorFont::bold) << "\n";
-    std::cout << color(ColorId::cyan, "    new", ColorFont::bold) << "  Create a new cask package\n";
-}
+//     std::cout << color(ColorId::green, "Commands:", ColorFont::bold) << "\n";
+//     std::cout << color(ColorId::cyan, "    new", ColorFont::bold) << "  Create a new cask package\n";
+// }
 
 int main(int ac, char *av[])
 {
-    if (ac < 2)
-    {
-        show_help();
-        return 1;
-    }
+    // if (ac < 2)
+    // {
+    //     show_help();
+    //     return 1;
+    // }
+
+    std::cout << "C++'s package manager\n\n";
 
     return 0;
 }
